dcuddeback/libudev-rs

Simplify API by removing Context
The libudev C library (now part of systemd) defines a context struct that
is passed to many functions. However, this context is not used by libudev,
but is a way for the client to include a pointer to some of its own
context with libudev objects, which might be handy for some programming
styles.

Currently, libudev-rs treats Context as if the pointer it wraps needs to be
passed to various API calls, but in fact these API calls accept NULL, and
this value is not used within libudev, other than accessor functions like
udev_{monitor,device,enumerate,queue}_get_udev(). libudev-rs doesn't
currently support setting the userdata pointer when creating Context,
or getting context values back.

Removing Context makes the API simpler not in just eliminating creating
the context as a step, but also because other libudev-rs structs that
needed a lifetime parameter because of a member referencing a &'a Context
no longer need one.

Finally, in removing Context, it necessary to move
Context::device_from_syspath() to Device::from_syspath().

Signed-off-by: Andy Grover <agrover@redhat.com>

examples/list_devices.rs
@@ -3,12 +3,11 @@ extern crate libudev;
 use std::io;
 
 fn main() {
-    let context = libudev::Context::new().unwrap();
-    list_devices(&context).unwrap();
+    list_devices().unwrap();
 }
 
-fn list_devices(context: &libudev::Context) -> io::Result<()> {
-    let mut enumerator = try!(libudev::Enumerator::new(&context));
+fn list_devices() -> io::Result<()> {
+    let mut enumerator = try!(libudev::Enumerator::new());
 
     for device in try!(enumerator.scan_devices()) {
         println!("");

Smithay/udev-rs

Signed-off-by: Dan Ravensloft <dan.ravensloft@gmail.com>

examples/list_devices.rs
@@ -3,12 +3,11 @@ extern crate udev;
 use std::io;
 
 fn main() {
-    let context = udev::Context::new().unwrap();
-    list_devices(&context).unwrap();
+    list_devices().unwrap();
 }
 
-fn list_devices(context: &udev::Context) -> io::Result<()> {
-    let mut enumerator = try!(udev::Enumerator::new(&context));
+fn list_devices() -> io::Result<()> {
+    let mut enumerator = try!(udev::Enumerator::new());
 
     for device in try!(enumerator.scan_devices()) {
         println!("");
@@ -25,8 +24,7 @@ fn list_devices(context: &udev::Context) -> io::Result<()> {
 
         if let Some(parent) = device.parent() {
             println!("     parent: {:?}", parent.syspath());
-        }
-        else {
+        } else {
             println!("     parent: None");
         }
 