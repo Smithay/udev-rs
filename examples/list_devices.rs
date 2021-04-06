67ad25b1e01642bc8d055013cddc65efbfadcbce
Patrick José Pereira <patrickelectric@gmail.com>
2021-04-06 16:49:13 -0300

    examples: Use new debug of Device
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- examples/list_devices.rs
+++ examples/list_devices.rs
@@ -7,22 +7,7 @@ fn main() -> io::Result<()> {
 
     for device in enumerator.scan_devices()? {
         println!();
-        println!("initialized: {:?}", device.is_initialized());
-        println!("     devnum: {:?}", device.devnum());
-        println!("    syspath: {:?}", device.syspath());
-        println!("    devpath: {:?}", device.devpath());
-        println!("  subsystem: {:?}", device.subsystem());
-        println!("    sysname: {:?}", device.sysname());
-        println!("     sysnum: {:?}", device.sysnum());
-        println!("    devtype: {:?}", device.devtype());
-        println!("     driver: {:?}", device.driver());
-        println!("    devnode: {:?}", device.devnode());
-
-        if let Some(parent) = device.parent() {
-            println!("     parent: {:?}", parent.syspath());
-        } else {
-            println!("     parent: None");
-        }
+        println!("{:#?}", device);
 
         println!("  [properties]");
         for property in device.properties() {
