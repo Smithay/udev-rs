14fd5860847edd73c197bc0ead7a410e6c5ce5ac
Ian Douglas Scott <idscott@system76.com>
2022-09-08 10:05:27 -0700

    Mark `ptr_to_os_str` as unsafe, and implement with `CStr::from_ptr`
    Dereferencing raw pointers can be unsound for reasons other than having
    a null pointer, so this function should be marked `unsafe`.
    
    Using `CStr::from_ptr` here is a bit shorter, and should have the same
    result, so it seems slightly better than calling `strlen` and
    `from_raw_parts`.

--- src/util.rs
+++ src/util.rs
@@ -1,24 +1,20 @@
-use std::ffi::{CString, OsStr};
+use std::ffi::{CStr, CString, OsStr};
 use std::io::Result;
-use std::slice;
 
 use libc::{c_char, c_int};
 
 use std::os::unix::prelude::*;
 
-pub fn ptr_to_os_str<'a>(ptr: *const c_char) -> Option<&'a OsStr> {
+pub unsafe fn ptr_to_os_str<'a>(ptr: *const c_char) -> Option<&'a OsStr> {
     if ptr.is_null() {
         return None;
     }
 
-    Some(unsafe { ptr_to_os_str_unchecked(ptr) })
+    Some(ptr_to_os_str_unchecked(ptr))
 }
 
 pub unsafe fn ptr_to_os_str_unchecked<'a>(ptr: *const c_char) -> &'a OsStr {
-    OsStr::from_bytes(slice::from_raw_parts(
-        ptr as *const u8,
-        libc::strlen(ptr) as usize,
-    ))
+    OsStr::from_bytes(CStr::from_ptr(ptr).to_bytes())
 }
 
 pub fn os_str_to_cstring<T: AsRef<OsStr>>(s: T) -> Result<CString> {
